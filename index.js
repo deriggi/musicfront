b9e13f2daa18acb5c006565ef36fc690bd0f9951
John DeRiggi <john.deriggi@gmail.com>
2020-07-21 17:42:25 -0700

    icons for artist, track or, album

--- index.js
+++ index.js
@@ -1,135 +1,145 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { SearchInput, Text, Switch, ApplicationsIcon, TickCircleIcon, Table, Pane } from 'evergreen-ui'
+import { SearchInput, Text, Switch, ApplicationsIcon, PersonIcon, Table, Pane } from 'evergreen-ui'
 
 import 'style.css';
 const API_BASE = 'http://localhost:8080';
 
-class DataPuller extends React.Component{
+class DataPuller extends React.Component {
 
-  constructor(props){
-      super(props)
-      this.state = {
-        showArtists:false, 
-        showAlbums:false, 
-        showTracks:false,
-        artists:[]}
+  constructor(props) {
+    super(props)
+    this.state = {
+      showArtists: true,
+      showAlbums: true,
+      showTracks: false,
+      artists: []
+    }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(`${API_BASE}/hi`)
-    .then(data =>data.json())
-    .then(asJson =>console.log(asJson));
+      .then(data => data.json())
+      .then(asJson => console.log(asJson));
   }
 
-  search(term){
-    if(term === undefined || term.length === 0){
-      return[];
+  search(term) {
+    if (term === undefined || term.length === 0) {
+      return [];
     }
     fetch(`${API_BASE}/search/${term}`)
-    .then(data =>data.json())
-    .then(asJson =>this.setState({ artists : asJson }));
+      .then(data => data.json())
+      .then(asJson => this.setState({ artists: asJson }));
   }
-  render(){
-    
+  render() {
+
     let profiles = this.state.artists;
+    let filtered = profiles.filter(p => this.state.showArtists && p.type == 'Artist' || this.state.showAlbums && p.type == 'Album')
+    
+    let searchResult = filtered.map(profile => {
+      let icon;
+      
+      if(profile.type == 'Artist'){
+        icon  =<PersonIcon color="muted" marginRight={16} />;
+      }else if (profile.type == 'Album'){
+        icon = <ApplicationsIcon color="muted" marginRight={16} />
+      }else{
+        icon = <ApplicationsIcon color="muted" marginRight={16} />
+      }
 
-
-    // profiles.push({id:1, name:"trey", lastActivity:new Date().toISOString(), ltv:3})
-    // profiles.push({id:2, name:"nadine", lastActivity:new Date().toISOString(), ltv:3})
-    // profiles.filter
-
-    let searchResult  = this.state.artists.map(profile => {
-      let icon = profile.type == 'Artist' ? <TickCircleIcon color="success" marginRight={16} /> : <ApplicationsIcon color="success" marginRight={16} />
       let row = <Table.Row key={profile.id} isSelectable onSelect={() => alert(profile.name)}>
-        <Table.TextCell>{profile.name===undefined?profile.title:profile.name}</Table.TextCell>
-        <Table.TextCell>{profile.lastActivity}</Table.TextCell>
+        <Table.TextCell>{profile.name === undefined ? profile.title : profile.name}</Table.TextCell>
+        {/* <Table.TextCell>{profile.lastActivity}</Table.TextCell> */}
         <Table.TextCell >
-        {icon}
+          {icon}
         </Table.TextCell>
       </Table.Row>
       return row;
     })
-  
 
-    let pane = 
-    <Pane  width={1000} margin="auto" >
+    
+
+    let pane =
+      <Pane width={1000} margin="auto" >
 
 
-<Table.SearchHeaderCell
-      height={20} margin={30}
-    onChange={value => this.search(value)}
-    placeholder='Search by artist, track, album...'
-  />
+        <Table.SearchHeaderCell
+          height={20} margin={30}
+          onChange={value => this.search(value)}
+          placeholder='Search by artist, track, album...'
+        />
 
-<Pane  margin={30} marginBottom={80} >
-        <Pane  display='flex' float="left" >
-          <Text  margin={10} marginRight={5} size={400}>Artists</Text>       
-          <Switch  
-            margin={10}
-            checked={this.state.showArtists}
-            onChange={e => this.setState({ showArtists: e.target.checked })}
-          />
+        <Pane margin={30} marginBottom={80} >
+          <Pane display='flex' float="left" >
+            <Text margin={10} marginRight={5} size={400}>Artists</Text>
+            <Switch
+              margin={10}
+              checked={this.state.showArtists}
+              onChange={e => this.setState({ showArtists: e.target.checked })}
+
+            />
+          </Pane>
+
+          <Pane display='flex' float="left" marginLeft={60}>
+            <Text margin={10} marginRight={5} size={400}>Albums</Text>
+            <Switch
+              margin={10}
+              checked={this.state.showAlbums}
+              onChange={e => {
+                this.setState({ showAlbums: e.target.checked })
+                console.log("state is ", this.state);
+              }}
+            />
+          </Pane>
+
+          <Pane display='flex' float="left" marginLeft={60}>
+            <Text margin={10} marginRight={5} size={400}>Tracks</Text>
+            <Switch
+              margin={10}
+              checked={this.state.showTracks}
+              onChange={e => this.setState({ showTracks: e.target.checked })}
+            />
+          </Pane>
+
         </Pane>
 
-        <Pane  display='flex' float="left" marginLeft={60}>
-          <Text margin={10} marginRight={5} size={400}>Albums</Text>      
-          <Switch  
-            margin={10}
-            checked={this.state.showAlbums}
-            onChange={e => this.setState({ showAlbums: e.target.checked })}
-          />
-        </Pane>
 
-        <Pane display='flex' float="left" marginLeft={60}>
-          <Text margin={10} marginRight={5} size={400}>Tracks</Text>      
-          <Switch  
-            margin={10}
-            checked={this.state.showTracks}
-            onChange={e => this.setState({ showTracks: e.target.checked })}
-          />
-        </Pane>
+        <Pane clearfix elevation={3} margin={12}>
 
+
+          <Table margin="30">
+            <Table.Head>
+              <Table.TextHeaderCell>
+                Name
+        </Table.TextHeaderCell>
+              {/* <Table.TextHeaderCell>
+                Last Activity
+        </Table.TextHeaderCell> */}
+              <Table.TextHeaderCell>
+                Type
+        </Table.TextHeaderCell>
+            </Table.Head>
+            <Table.Body>
+              {searchResult}
+            </Table.Body>
+          </Table>
+
+
+
+        </Pane>
       </Pane>
-
-
-      <Pane clearfix elevation={3}  margin={12}>
-      
-
-      <Table margin="30">
-      <Table.Head>
-        {/* <Table.SearchHeaderCell /> */}
-        <Table.TextHeaderCell>
-          Name
-        </Table.TextHeaderCell>
-        <Table.TextHeaderCell>
-          Last Activity
-        </Table.TextHeaderCell>
-        <Table.TextHeaderCell>
-          ltv
-        </Table.TextHeaderCell>
-      </Table.Head>
-      <Table.Body>
-      {searchResult}
-      </Table.Body>
-    </Table>
-
-     
-
-    </Pane>
-  </Pane>
-  return pane
+    return pane
   }
 
 
-   sayWhat(w){
+  sayWhat(w) {
     console.log("i said what " + w);
   }
 
-  
-  
+
+
 }
 ReactDOM.render(
-  <DataPuller/>,
+  <DataPuller />,
   document.getElementById('root')
 )
